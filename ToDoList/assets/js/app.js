aafba110e0618d6b7999be21008ab98495d61052
unknown <gwendoline.richaut@gmail.com>
2020-03-24 16:12:53 +0100

    checkbox

--- ToDoList/assets/js/app.js
+++ ToDoList/assets/js/app.js
@@ -13,19 +13,19 @@ const options = { weekday: 'long',  month: 'long', day: 'numeric' };
 
 document.getElementById("date").innerHTML= dateNow.toLocaleDateString("fr", options)
 
-//document.getElementById("date").innerHTML= dateNow.getDate()+"/"+(dateNow.getMonth()+1)+"/"+dateNow.getFullYear()
 
 //const liste = document.getElementById("liste");
 
 let input = document.getElementById("input")
 
+/*
 let liste = document.querySelector("ul")
 
 const ajoutListe = (input) => {
         ligne = document.createElement("li")
         liste.appendChild(ligne)
         ligne.innerHTML= input
-}
+}*/
 
 document.getElementById("btn").addEventListener("click", function () {
 
@@ -42,14 +42,15 @@ document.getElementById("btn").addEventListener("click", function () {
             <img src="./assets/image/poubelle.png" alt="Enregistrer" title="Enregistrer" class="bin" />
         </div>
       </div>
-    <i class="supprimer"></i>
 </li>
     `
      )
 
 })
 
-//document.write(dateNow.getDate()+"/"+(dateNow.getMonth()+1)+"/"+dateNow.getFullYear())
+//document.getElementById("btn").addEventListener("click", function () {
+
+
 
 //stocker un item
 localStorage.setItem("nomValeur", "truc à stocker")
@@ -58,13 +59,4 @@ localStorage.getItem("nomValeur")
 //vider le stockage
 localStorage.clear()
 
-const btn = document.querySelector(".btn");
 
-btn.addEventListener("click", function(){
-
-} )
-
-
-//var ladate=new Date()
-//document.write("Nous sommes le : ");
-//document.write(ladate.getDate()+"/"+(ladate.getMonth()+1)+"/"+ladate.getFullYear())
