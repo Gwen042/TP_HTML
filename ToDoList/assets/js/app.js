bfa271663721f1e74259ba814e6b19c45127225a
unknown <gwendoline.richaut@gmail.com>
2020-03-26 08:57:43 +0100

    tp todo delete en cours

--- ToDoList/assets/js/app.js
+++ ToDoList/assets/js/app.js
@@ -1,3 +1,5 @@
+document.getElementById('imgDate').src = "./assets/image/date/image" + Math.floor(Math.random() * 10) + ".jpg";
+
 const clear = document.querySelector(".clear")
 
 clear.addEventListener("click", function(){
@@ -37,16 +39,36 @@ document.getElementById("btn").addEventListener("click", function () {
     <li>
       <input type="checkbox" id="checkbox" class="checkbox" />
       <label for="checkbox"> ${toDo}</label>
-      <img src="./assets/image/poubelle.png" id="bin" class="bin" />
+      <img src="./assets/image/poubelle.png" class="bin" />
     </li>
     `
      )
 
 })
 
+//supprimer
+
+let bin = document.getElementsByClassName("bin")
+
+//binRemove = () =>{
+for(var i = 0; i < bin.length; i++) {
+    (function(){
+        let d = bin[i].parentNode
+        bin[i].addEventListener('click', function(){
+        d.remove()})
+        //console.log(d)
+    }());
+};
+//}
+
+
+//binRemove()
+
+/*
 //stocker un item
 localStorage.setItem("nomValeur", "truc à stocker")
 //récupérer un item
 localStorage.getItem("nomValeur")
 //vider le stockage
 localStorage.clear()
+*/
